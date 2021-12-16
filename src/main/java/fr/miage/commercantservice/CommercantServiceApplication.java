f3c33edc65ff73b4e21d05570daa95808f5a80d0
maxim <maxim.marigliano@gmail.com>
2021-12-16 18:21:19 +0100

    first commit

---
+++ src/main/java/fr/miage/commercantservice/CommercantServiceApplication.java
@@ -0,0 +1,13 @@
+package fr.miage.commercantservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommercantServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommercantServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/miage/commercantservice/CommercantServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.miage.commercantservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommercantServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
